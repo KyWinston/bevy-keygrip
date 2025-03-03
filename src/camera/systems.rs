8ab2f87c1482925dc088ae56fc308e00fc6ad32c
KyleWinston <kyle.w.r@hotmail.com>
2025-03-02 20:01:59 -0500

    update tracking

--- src/camera/systems.rs
+++ src/camera/systems.rs
@@ -7,17 +7,13 @@ use super::components::{CameraDriver, PlayerCamera};
 pub fn update_camera_position(
     mut cam: Query<(&mut Transform, &Grip), With<PlayerCamera>>,
     target: Query<&GlobalTransform, With<CameraDriver>>,
-    time: Res<Time<Fixed>>,
 ) {
-    let a = time.overstep_fraction();
     if let Ok((mut transform, grip)) = cam.get_single_mut() {
         if let Ok(t) = target.get_single() {
             let future_position = t.translation()
                 + *t.back() * grip.location_offset.z
                 + *t.up() * grip.location_offset.y;
-            transform.translation = transform
-                .translation
-                .lerp(future_position, grip.tracking.0 * a);
+            transform.translation = transform.translation.lerp(future_position, grip.tracking.0);
         }
     }
 }
