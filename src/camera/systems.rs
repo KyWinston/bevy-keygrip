2ab494e6d95777e92295e8987096d547d5a8a0b1
KyleWinston <kyle.w.r@hotmail.com>
2025-03-08 03:10:23 -0500

    tracking

--- src/camera/systems.rs
+++ src/camera/systems.rs
@@ -2,21 +2,19 @@ use bevy::prelude::*;
 
 use crate::components::Grip;
 
-use super::components::{CameraDriver, PlayerCamera};
+use super::components::PlayerCamera;
 
 pub fn update_camera_position(
-    mut cam: Query<(&mut Transform, &Grip), With<PlayerCamera>>,
-    target: Query<&GlobalTransform, With<CameraDriver>>,
+    mut cam: Query<(&mut Transform, &Parent, &Grip), With<PlayerCamera>>,
+    target: Query<(Entity, &Transform), Without<PlayerCamera>>,
 ) {
-    if let Ok((mut transform, grip)) = cam.get_single_mut() {
-        if let Ok(t) = target.get_single() {
-            let distance = transform.translation.distance(t.translation());
-            let future_position = t.translation()
-                + *t.back() * grip.location_offset.z
-                + *t.up() * grip.location_offset.y;
+    if let Ok((mut transform, parent, grip)) = cam.get_single_mut() {
+        if let Ok(t) = target.get(**parent) {
+            let future_position = grip.location_offset;
+
             transform.translation = transform.translation.lerp(
                 future_position,
-                EasingCurve::new(0.2, 1.0, grip.smoothing_curve)
+                EasingCurve::new(0.3, 1.0, grip.smoothing_curve)
                     .sample(grip.tracking.0)
                     .unwrap_or(0.0),
             );
@@ -25,13 +23,16 @@ pub fn update_camera_position(
 }
 
 pub fn update_camera_rotation(
-    mut cam: Query<(&mut Transform, &Grip), With<PlayerCamera>>,
-    target: Query<&GlobalTransform, With<CameraDriver>>,
+    mut cam: Query<(&mut Transform, &Parent, &Grip), With<PlayerCamera>>,
+    target: Query<(Entity, &Transform), Without<PlayerCamera>>,
 ) {
-    if let Ok((mut transform, grip)) = cam.get_single_mut() {
-        if let Ok(t) = target.get_single() {
+    if let Ok((mut transform, parent, grip)) = cam.get_single_mut() {
+        if let Ok((t, t_transform)) = target.get(**parent) {
             let rotation = transform
-                .looking_at(t.translation() + t.up() * grip.rotation_offset.y, t.up())
+                .looking_at(
+                    t_transform.translation + t_transform.up() * grip.rotation_offset.y,
+                    t_transform.up(),
+                )
                 .rotation;
             if transform.rotation != rotation {
                 transform.rotation = transform.rotation.lerp(rotation, grip.tracking.1);
